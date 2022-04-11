5f7256b6cbd8fc88292661b3d8bc1f3b79d34090
renoldiboz <renoldiboz@gmail.com>
2022-04-11 20:50:57 +0700

    change

--- app/Config/Format.php
+++ app/Config/Format.php
@@ -56,7 +56,7 @@ class Format extends BaseConfig
      * @var array<string, int>
      */
     public $formatterOptions = [
-        'application/json' => JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES,
+        'application/json' => JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK,
         'application/xml'  => 0,
         'text/xml'         => 0,
     ];
